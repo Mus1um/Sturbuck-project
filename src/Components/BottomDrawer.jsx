4e5c57c733407e6795b8b3ed6aafe927f5566dfe
muslummemmedzadee@gmail.com <muslummemmedzadee@gmail.com>
2024-03-30 23:49:00 +0400

    menuuu

--- src/Components/BottomDrawer.jsx
+++ src/Components/BottomDrawer.jsx
@@ -1,47 +1,64 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import '../Style/BottomDrawer.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Style/BottomDrawer.css";
 import { SlArrowDown } from "react-icons/sl";
-import { IoBagOutline } from "react-icons/io5";
+
+import { IoBag } from "react-icons/io5";
 
 function BottomDrawer() {
-    const [selected,setSelected]=useState(false)
-    const handleselected=()=>{
-     
-   setSelected(!selected)
-    
-        document.body.style.overflow = selected ? "visible" : "hidden";
-     
+  const [selected, setSelected] = useState(false);
+  const [btnClick, setBtnClick] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+  const handleselected = () => {
+    setSelected(!selected);
+
+    document.body.style.overflow = selected ? "visible" : "hidden";
+  };
+  const handleBtnClick = () => {
+    setBtnClick(true)
+    if ( clickCount < 15) {
+        setClickCount(click=>click+1)  
+    }else{
+        alert('Maximum order is 15 items. Please adjust your order.')
     }
+  };
+
   return (
-    <div className='bottomdrawer'>
-        <div className="modal" style={{display:selected ? 'flex' :'none'}}>
-            <div className="box" style={{transform:selected ? 'scale(1)' :''}}>
-                <p>Please select a store before continuing to the cart.</p>
-                <div className="buttons">
-                    <Link onClick={handleselected}>Cancel</Link>
-                    <Link>Select Store</Link>
-                </div>
-            </div>
+    <div className="bottomdrawer">
+      <div className="modal" style={{ display: selected ? "flex" : "none" }}>
+        <div className="box" style={{ transform: selected ? "scale(1)" : "" }}>
+          <p>Please select a store before continuing to the cart.</p>
+          <div className="buttons">
+            <Link onClick={handleselected}>Cancel</Link>
+            <Link>Select Store</Link>
+          </div>
         </div>
-        <div className="drawer" style={{display:selected? 'none':'flex'}}>
-            <div className="left">
-         <Link>
-         <p>
-         <span>For item availability</span>
-         <strong>Choose a store</strong>
-         </p>
-         <SlArrowDown className='sl'/>
-         </Link>
+      </div>
+      <div className="drawer" style={{ display: selected ? "none" : "flex" }}>
+        <div className="left">
+          <Link>
+            <p>
+              <span>For item availability</span>
+              <strong>Choose a store</strong>
+            </p>
+            <SlArrowDown className="sl" />
+          </Link>
         </div>
         <div className="right">
-       <button onClick={handleselected}><IoBagOutline className='bag' /></button>
+          <h3>{clickCount>0? clickCount :''}</h3>
+          <button onClick={handleselected}>
+            <IoBag
+              className="bag"
+              style={{ color: btnClick ? "#00a862" : "" }}
+            />
+          </button>
         </div>
-        </div>
-        
-      
+      </div>
+      <button onClick={handleBtnClick} className="btn-add">
+        Add to Order
+      </button>
     </div>
-  )
+  );
 }
 
-export default BottomDrawer
+export default BottomDrawer;
--- src/Components/InsideProduct.jsx
+++ src/Components/InsideProduct.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import "../Style/InsideProduct.css";
 import { SlLocationPin } from "react-icons/sl";
+import { GiFairyWand } from "react-icons/gi";
 import size12 from "../Images/size12.svg";
 import size14 from "../Images/size14.svg";
 import size16 from "../Images/size16.svg";
+import size20 from "../Images/size20.svg";
 import size12active from "../Images/size12active.svg";
 import size14active from "../Images/size14active.svg";
 import size16active from "../Images/size16active.svg";
+import size20active from "../Images/size20active.svg";
 
 function InsideProduct(props) {
   const data = props.data;
@@ -15,8 +18,8 @@ function InsideProduct(props) {
   const handleSizeClick = (index) => {
     setActive(index);
   };
-  const images = [size12, size14, size16];
-  const imagesActive = [size12active, size14active, size16active];
+  const images = [size12, size14, size16, size20];
+  const imagesActive = [size12active, size14active, size16active, size20active];
 
   return (
     <div>
@@ -38,8 +41,11 @@ function InsideProduct(props) {
                   key={index}
                   onClick={() => handleSizeClick(index)}
                 >
+                  <div
+                    className="active"
+                    style={{ display: index === active ? "block" : "none" }}
+                  ></div>
                   <img
-                    className={`${index === active ? "active" : ""}`}
                     src={index === active ? imagesActive[index] : images[index]}
                     alt=""
                   />
@@ -54,6 +60,90 @@ function InsideProduct(props) {
           </div>
           <div className="product-right">
             <h1>What's included</h1>
+            <div className="customized">
+              <form>
+                <fieldset>
+                  <legend>Milk</legend>
+                  <select name="" id="">
+                    <option value="">
+                      <p>Milk Foam</p>
+                    </option>
+                    <option value="">
+                      <p>Extra Milk Foam</p>
+                    </option>
+                    <option value="">
+                      <p>Light Milk Foam</p>
+                    </option>
+                    <option value="">
+                      <p>No Milk Foam</p>
+                    </option>
+                  </select>
+                </fieldset>
+                <fieldset>
+                  <legend>Milk</legend>
+                  <select name="" id="">
+                    <option value="">
+                      <p>Heavy Cream</p>
+                    </option>
+                    <option value="">
+                      <p>Vanilla Sweet Cream</p>
+                    </option>
+                    <option value="">
+                      <p>2% Milk</p>
+                    </option>
+                    <option value="">
+                      <p>Whole Milk</p>
+                    </option>
+                  </select>
+                </fieldset>
+                <fieldset>
+                  <legend>Milk</legend>
+                  <select name="" id="">
+                    <option value="">
+                      <p>Warm</p>
+                    </option>
+                    <option value="">
+                      <p>Steamed Hot</p>
+                    </option>
+                    <option value="">
+                      <p>Extra Hot</p>
+                    </option>
+                  </select>
+                </fieldset>
+                <fieldset>
+                  <legend>Add-ins</legend>
+                  <select name="" id="">
+                    <option value="">
+                      <p>Press of Partanna® Olive Oil</p>
+                    </option>
+                  </select>
+                </fieldset>
+                <fieldset>
+                  <legend>Espresso & Shot Options</legend>
+                  <select name="" id="">
+                    <option value="">
+                      <p>Signature Espresso Roast</p>
+                    </option>
+                    <option value="">
+                      <p>Blonde Espresso Roast</p>
+                    </option>
+                    <option value="">
+                      <p>Decaf Espresso Roast</p>
+                    </option>
+                    <option value="">
+                      <p>1/3 Decaf Espresso Roast</p>
+                    </option>
+                    <option value="">
+                      <p>1/2 Decaf Espresso Roast</p>
+                    </option>
+                    <option value="">
+                      <p>2/3 Decaf Espresso Roast</p>
+                    </option>
+                  </select>
+                </fieldset>
+              </form>
+              <button className="btn-customize"> <GiFairyWand className="btn-icon" /> Customize</button>
+            </div>
           </div>
         </div>
       </main>
